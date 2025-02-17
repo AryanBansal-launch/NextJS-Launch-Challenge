c56f8a8a39049d60aeb0fb07b14bf57858f8aa63
Aryan Bansal <aryan.bansal@contentstack.com>
2025-02-17 23:47:39 +0530

    point 3 done

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+  timestamp: string; // Extra field
+}
+type ErrorResponse = { error: string };
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Post | ErrorResponse>
+) {
+  const { id } = req.query;
+  try {
+    const url = `https://jsonplaceholder.typicode.com/posts/${id}`;
+    const response = await fetch(url);
+    const data:Post = await response.json();
+    const timestamp = new Date().toISOString();
+    res.status(200).json({ ...data, timestamp });
+  } catch (error) {
+    console.error('Error fetching post:', error);
+    res.status(500).json({ error: 'Failed to fetch post' });
+  }
+}
