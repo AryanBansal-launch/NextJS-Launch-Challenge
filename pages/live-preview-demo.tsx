ca62254d4d16af5bf79f0c4f6cd31db4873fb506
Aryan Bansal <aryan.bansal@contentstack.com>
2025-02-19 18:25:45 +0530

    fixed type issue

--- pages/live-preview-demo.tsx
+++ pages/live-preview-demo.tsx
@@ -1,25 +1,23 @@
- // page.js
- import { headers } from "next/headers";
+// page.js
 import { setLivePreviewQueryParams } from "../utils";
- import LivePreviewInitComponent from "./components/LivePreviewInitComponent"
-import { GetServerSideProps } from 'next';
+import LivePreviewInitComponent from "./components/LivePreviewInitComponent";
 import { getContentRes } from "../utils";
-import { get } from "http";
 import { useRouter } from "next/router";
 
 export default async function Page() {
   const router = useRouter();
   const { live_preview } = router.query;
-   setLivePreviewQueryParams(live_preview as any);
-   const ContentData = await getContentRes();
-   console.log("Content Data:", ContentData);
-   return (
-     <>
-       <h1>
-         Content Type Title: {" " + ContentData?.title }
-       </h1>
-       <LivePreviewInitComponent />
-      </>
-   );
- }
- 
+  if (live_preview !== undefined) {
+    if (typeof live_preview === 'string') {
+      setLivePreviewQueryParams({ live_preview });
+    }
+  }
+  const ContentData = await getContentRes();
+  console.log("Content Data:", ContentData);
+  return (
+    <>
+      <h1>Content Type Title: {" " + ContentData?.title}</h1>
+      <LivePreviewInitComponent />
+    </>
+  );
+}
