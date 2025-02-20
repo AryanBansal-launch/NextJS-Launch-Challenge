4e56a14c8760a388801d0b37096008d8de56e4cc
Aryan Bansal <aryan.bansal@contentstack.com>
2025-02-20 18:22:41 +0530

    remoevd revalidate=0

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  headers: async () => {
-    return [
-      {
-        source: "/posts/:id",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "s-maxage=10, stale-while-revalidate=0",
-          },
-        ],
-      },
-    ];
-  },
   reactStrictMode: true,
 };
 
