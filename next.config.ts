3788aceb428e4afb841e84a5f5e846e5b664c9ad
Aryan Bansal <aryan.bansal@contentstack.com>
2025-02-20 18:29:58 +0530

    changed config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  headers: async () => {
+    return [
+      {
+        source: "/posts/:id",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "stale-while-revalidate=0",
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
 };
 
