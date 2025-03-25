9b73dc4567a0da3b65999334f5be0eeca88b23ae
Aryan Bansal <aryan.bansal@contentstack.com>
2025-03-25 17:07:28 +0530

    s-maxage=10

--- pages/api/cache-test.ts
+++ pages/api/cache-test.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader('Cache-Control', 'public, max-age=30, s-maxage=60, stale-while-revalidate=3600');
+  res.setHeader('Cache-Control', 'public, max-age=30, s-maxage=10, stale-while-revalidate=3600');
   res.status(200).json({
     message: "Testing Cache Behavior",
     timestamp: new Date().toISOString(),
