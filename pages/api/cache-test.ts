3c1fc9e05fbe9dca426f1975a6935b8af2a6cd3b
Aryan Bansal <aryan.bansal@contentstack.com>
2025-03-25 16:38:03 +0530

    testing cache headers

---
+++ pages/api/cache-test.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Cache-Control', 'public, max-age=30, s-maxage=60, stale-while-revalidate=3600');
+  res.status(200).json({
+    message: "Testing Cache Behavior",
+    timestamp: new Date().toISOString(),
+  });
+}
