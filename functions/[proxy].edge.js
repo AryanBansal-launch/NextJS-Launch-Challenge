42eb3f76e09a9dfd7101286d446093d4f604b98a
Aryan Bansal <aryan.bansal@contentstack.com>
2025-07-16 16:01:38 +0530

    added edge fn to block crawler

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,12 +1,24 @@
+// export default function handler(request, context) {
+//   // console.log('edge API called')
+//     const parsedUrl = new URL(request.url);
+//     const route = parsedUrl.pathname;
+//     if (route === '/appliances') {
+//       const response = {
+//         time: new Date()
+//       }
+//       return new Response(JSON.stringify(response))
+//     }
+//     return fetch(request)
+// } 
+
+const KNOWN_BOTS = ['ClaudeBot', 'GPTBot', 'Googlebot', 'Bingbot', 'AhrefsBot'];
+
 export default function handler(request, context) {
-  // console.log('edge API called')
-    const parsedUrl = new URL(request.url);
-    const route = parsedUrl.pathname;
-    if (route === '/appliances') {
-      const response = {
-        time: new Date()
-      }
-      return new Response(JSON.stringify(response))
+  const userAgent = request.headers.get('user-agent') || '';
+  for (const bot of KNOWN_BOTS) {
+    if (userAgent.includes(bot)) {
+      return new Response('Forbidden: AI crawlers are not allowed.', { status: 403 });
     }
-    return fetch(request)
+  }
+  return fetch(request);
 } 
