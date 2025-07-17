fd3a72093fc8a190b21e83673a485716feb871b3
Aryan Bansal <aryan.bansal@contentstack.com>
2025-07-17 23:37:39 +0530

    rediercts test

---
+++ pages/test1/index.tsx
@@ -0,0 +1,10 @@
+const index = () => {
+    return (
+      <div>
+        <h1>Test 1</h1>
+      </div>
+    )
+  }
+  
+  export default index
+  
---
+++ pages/test2/index.tsx
@@ -0,0 +1,9 @@
+const index = () => {
+  return (
+    <div>
+      <h1>Test 2</h1>
+    </div>
+  )
+}
+
+export default index
