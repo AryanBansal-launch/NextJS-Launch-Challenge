279644c6427979743b8c6c8ea68733d1b6c8096c
AryanBansal-launch <aryan.bansal@contentstack.com>
2025-09-11 16:34:56 +0530

    Update [proxy].edge.js in main

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,21 +1,23 @@
 export default async function handler(request) {
-  const timestamp = Date.now();
-  console.log('edge API called', 'Time:', timestamp);
+  // const timestamp = Date.now();
+  // console.log('edge API called', 'Time:', timestamp);
 
-  const parsedUrl = new URL(request.url);
-  const route = parsedUrl.pathname;
+  // const parsedUrl = new URL(request.url);
+  // const route = parsedUrl.pathname;
 
-  if (route === '/appliances') {
-    const response = {
-      time: new Date().toISOString()
-    };
-    return new Response(JSON.stringify(response), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-
+  // if (route === '/appliances') {
+  //   const response = {
+  //     time: new Date().toISOString()
+  //   };
+  //   return new Response(JSON.stringify(response), {
+  //     headers: { 'Content-Type': 'application/json' },
+  //   });
+  // }
+  const res= await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const finalres=await res.json();
+  return finalres;
   // Fallback: proxy the request
-  return fetch(request);
+  // return fetch(request);
 }
 
 
